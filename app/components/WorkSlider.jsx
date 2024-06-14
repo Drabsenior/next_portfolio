de87b0d9dc50046ca4fd31441d5a3ed4503a7fbc
Abenezer Maregu <99409488+Drabsenior@users.noreply.github.com>
2024-06-14 11:09:10 +0300

    replace projecs

--- app/components/WorkSlider.jsx
+++ app/components/WorkSlider.jsx
@@ -16,28 +16,28 @@ const workSlides = {
     {
       images: [
         {
-          title: "title",
-          path: "/thumb4.jpg",
-          source: "",
-          link: "https://kezyasv2.onrender.com/",
+          title: "Kezyas",
+          path: "/kezyascover.png",
+          source: "https://github.com/Drabsenior/",
+          link: "https://kezyas.com/",
         },
         {
-          title: "title",
-          path: "/thumb1.jpg",
-          source: "",
-          link: "https://plusova.com/",
+          title: "shopadmin",
+          path: "/shopadmincover.png",
+          source: "https://github.com/Drabsenior/",
+          link: "https://shop-admin-wpm8.onrender.com/",
         },
         {
-          title: "title",
-          path: "/thumb2.jpg",
-          source: "",
-          link: "https://imdb-oh67.vercel.app/",
+          title: "oddo custom",
+          path: "/oddoerpcover.png",
+          source: "https://github.com/Drabsenior/",
+          link: "https://erp.shegapps.com",
         },
         {
-          title: "title",
-          path: "/thumb3.jpg",
-          source: "",
-          link: "https://iat-appointment.onrender.com/",
+          title: "Chat room",
+          path: "/chatroomcover.png",
+          source: "https://github.com/Drabsenior/",
+          link: "https://atlas-chat.shegapps.com",
         },
       ],
     },
@@ -56,19 +56,20 @@ const WorkSlider = () => {
     >
       {workSlides.slides.map((slide, index) => {
         return (
-          <SwiperSlide key={index}>
+          <SwiperSlide key={index} >
             <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
               {slide.images.map((image, index) => {
                 return (
+                  <Link href={image?.link} key={index} target="_blank" rel="noopener noreferrer">
                   <div
                     className="relative rounded-lg overflow-hidden flex items-center justify-center group"
-                    key={index}
-                  >
+                    
+                    >
                     <div className="flex items-center justify-center relative overflow-hidden ">
                       {/* image */}
                       <Image src={image.path} width={500} height={300} alt="" />
                       {/* overlay gradient */}
-                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-70 transition-all duration-700"></div>
+                      <div className="absolute inset-0 bg-gradient-to-l from-[#e2a6d8] via-[#e838cc] to-[#4a22bd] opacity-10 group-hover:opacity-70 transition-all duration-700"></div>
                       {/* links to source and web */}
                       <Link href={image.source ? image.source : ""} target="_blank" rel="noopener noreferrer">
                         <h2 className="absolute bottom-2 cursor-pointer   right-4 duration-700 tracking-wide font-mono  hover:text-red-600 hover:scale-125 hover:font-bold transition-all ">
@@ -85,9 +86,11 @@ const WorkSlider = () => {
                         <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
                           {/* title part 1 */}
                           <div className="flex">
-                            <div className="delay-100 ">LIVE</div>
+                            <div className="delay-100 ">LIVE </div>
                             <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
-                              PROJECT
+                              <span className="px-2 uppercase">
+                              {image.title}
+                              </span>
                             </div>
                             <div className="text-xl translate-y-[600%] group-hover:translate-y-0 transition-all duration-300 delay-200">
                               <BsArrowRight />
@@ -97,6 +100,7 @@ const WorkSlider = () => {
                       </div>
                     </div>
                   </div>
+                    </Link>
                 );
               })}
             </div>
